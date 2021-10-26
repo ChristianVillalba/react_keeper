aa21ed3d19ca729e47340befba8eedddb5d10afd
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-10-26 14:47:02 +0100

    adds State to Title and Content (Note's Props) App.jsx : Add props to Note: id, title & content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteContent, setNoteContent] = useState("");
+
+  function handleTitleChange(event) {
+    const newValue = event.target.value;
+    setNoteTitle(newValue);
+  }
+  function handleContentChange(event) {
+    const newValue = event.target.value;
+    setNoteContent(newValue);
+  }
+
   return (
     <div>
       <Header />
       <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <Note key={index} id={index} title={noteTitle} content={noteContent} />
       <Footer />
     </div>
   );
