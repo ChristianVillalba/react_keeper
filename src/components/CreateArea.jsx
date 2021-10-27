2d4ae52ffaa8159a47c98820f1691dd707c774c7
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-10-27 23:51:19 +0100

    sets Content (Note) adds State to Content, function that handles changes in Content & what should be returned. ready to set button

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -22,7 +22,13 @@ function CreateArea(props) {
           name="title"
           placeholder="Title"
         />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <textarea
+          onChange={handleContentChange}
+          value={noteContent}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button>Add</button>
       </form>
     </div>
